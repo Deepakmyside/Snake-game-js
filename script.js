b1407511a7285018db3e2598587f21347d18fe7f
Deepak Sharma <deepakmyside@gmail.com>
2026-03-19 11:06:28 +0530

    feat: add start game, restart modal. Added random food generation , restart functionality

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // HTML me joh board class wala div h usko select kr rhe h taki uspar changes kr ske 
 const board = document.querySelector('.board')
+const startButton = document.querySelector('.btn-start')
+const modal = document.querySelector('.modal')
+const startGameModal = document.querySelector('.start-game')
+const gameOverModal = document.querySelector('.game-over')
+const restartButton = document.querySelector('.btn-restart')
  const blockHeight = 50
  const blockWidth =  50
 
@@ -8,17 +13,14 @@ const board = document.querySelector('.board')
  const rows = Math.floor(board.clientHeight / blockHeight);
 
  const blocks = []
- const snake = [ 
+ let snake = [ 
    {
    x: 1, y: 3
- },{
-   x: 1, y: 4
- },{
-   x :1, y: 5  
  } ]
 
  let direction = 'down'
-
+  let intervalId = null;
+  let food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)}
 
 
 for (let row = 0; row < rows; row++){
@@ -32,17 +34,9 @@ for (let row = 0; row < rows; row++){
 }
 
 function render(){
+       let head = null
 
-   snake.forEach(segment=> {
-      blocks[ `${segment.x}-${segment.y}` ].classList.add("fill")
-   })
-}
-
-// setInterval function means har 300(millisecond time) ke bad render function automatically call ho jayega 
-
-setInterval(() => {
-      
-     let head = null
+       blocks[`${food.x}-${food.y}`].classList.add("food")
 
      if(direction === "left"){
      head = { x: snake[ 0 ].x, y: snake[ 0 ].y - 1  }
@@ -54,15 +48,68 @@ setInterval(() => {
       head = { x: snake[ 0 ].x - 1 ,y: snake[ 0 ].y }
     }
 
-     
+     if(head.x<0 || head.x >= rows || head.y<0 || head.y>=cols) {
+      clearInterval(intervalId)
+      modal.style.display="flex";
+      startGameModal.style.display="none";
+      gameOverModal.style.display="flex";
+      return;
+     }
+
+     if(head.x == food.x && head.y == food.y){
+            blocks[`${food.x}-${food.y}`].classList.remove("food")
+
+            food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)
+            }
+            blocks[`${food.x}-${food.y}`].classList.add("food")
+            snake.unshift(head)
+     }
+
+
      snake.forEach(segment => {
       blocks[ `${segment.x}-${segment.y}` ].classList.remove("fill")
      })
 
      snake.unshift(head)
      snake.pop()
-   render()
-}, 200);
+
+   snake.forEach(segment=> {
+      blocks[ `${segment.x}-${segment.y}` ].classList.add("fill")
+   })
+}
+
+// setInterval function means har 300(millisecond time) ke bad render function automatically call ho jayega 
+
+// intervalId = setInterval(() => {
+      
+   
+//    render()
+// }, 300);
+
+ startButton.addEventListener("click", () => {
+  modal.style.display="none"
+  intervalId= setInterval(() => {render()}, 300)
+ } )
+
+ restartButton.addEventListener("click", restartGame)
+
+ function restartGame(){
+  
+  blocks[`${food.x}-${food.y}`].classList.remove("food")
+  snake.forEach(segment => {
+    blocks[ ` ${segment.x}-${segment.y}` ].classList.remove("fill")
+  })
+
+  modal.style.display="none";
+  
+  snake = [ {
+    x: 1, y: 3
+  }]
+  food= { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)}
+   intervalId = setInterval(() => {render( )}, 300)
+ }
+
+
 
 addEventListener("keydown",(event)=>{
      if(event.key=="ArrowUp"){
